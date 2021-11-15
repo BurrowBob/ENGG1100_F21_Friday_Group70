286f51ca0375969c9fe013f4bd098b8848eb763b
BurrowBob <93230170+BurrowBob@users.noreply.github.com>
2021-11-14 22:18:40 -0500

    Add files via upload

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -1,5 +1,5 @@
 /*
-    This program is an encryption/decryption utility called cryptoMagic
+    This program is an encryption/decryption utility called cryptoMagic see a more in depth description in README.txt
     Written By: William Rocheleau, Gabe Bigas
     Date: 14/11/21
 */
@@ -7,6 +7,8 @@
 #include <string.h>
 
 void encrypt(char base[], char filename[]);
+
+void decrypt(char base[], char filename[]);
 
 int main(int argc, char *argv[]){
     int fileNameLen;
@@ -69,7 +71,8 @@ int main(int argc, char *argv[]){
         printf("Error: please specify either encryption or decryption and a file name");
     }
 
-    if (strcmp(argv[1], "-encrypt") == 0){
+    //if -encrypt was specified along with a file name
+    else if (strcmp(argv[1], "-encrypt") == 0){
         int hasExt = 0;
         
         //find length of base file name (no extension) and if the filename has an extension
@@ -110,6 +113,53 @@ int main(int argc, char *argv[]){
             strcat(filename, ".txt\0");
 
             encrypt(fileNameBase, filename);
+        }
+
+        return 0;
+    }
+
+    //if -decrypt was specified along with a filename
+    else if (strcmp(argv[1], "-decrypt") == 0){
+        int hasExt = 0;
+        
+        //find length of base file name (no extension) and if the filename has an extension
+        for (int x = 0; x < strlen(argv[2]); x++){
+            if (argv[2][x] == '.'){
+                fileNameLen = x;
+                hasExt = 1;
+            }
+        }
+
+        if (hasExt==0)
+            fileNameLen = strlen(argv[2]);
+
+        //creates array to store base file name
+        char fileNameBase[fileNameLen+1];
+
+        //fills base file name array
+        for (int a = 0; a < fileNameLen; a++){
+            fileNameBase[a] = argv[2][a];
+        }
+
+        //adds eos operator to base name
+        fileNameBase[fileNameLen]='\0';
+
+        //calls encryption function using argv[2] if it has an extension
+        if (hasExt == 1){
+            decrypt(fileNameBase, argv[2]);
+        }
+        
+        //adds .txt extension if one was not given
+        else{
+            char filename[strlen(fileNameBase)+5];
+            
+            int i;
+            for (i = 0; i < strlen(fileNameBase); i++)
+                filename[i]=fileNameBase[i];
+            
+            strcat(filename, ".crp\0");
+
+            decrypt(fileNameBase, filename);
         }
 
         return 0;
@@ -172,4 +222,123 @@ void encrypt(char base[], char filename[]){
     
     fclose(input);
     fclose(output);
+}
+
+void decrypt(char base[], char filename[]){
+    FILE *fInput;
+    FILE *fOutput;
+    char line[255];
+    int x, ascii, skipLine = 0;
+    int pair[2];
+
+    //ask user for file name
+
+    fInput = fopen(filename, "r");
+
+    char fullName[strlen(base)+5];
+    strcpy(fullName, base);  
+    strcat(fullName, ".txt\0");
+
+    fOutput = fopen(fullName, "w");
+    fOutput = fopen(fullName, "a");
+    
+    while (!feof(fInput))
+    {
+        fgets(line, 255, (FILE *)fInput);
+        x = 0;
+        while (x < 255 && skipLine == 0)
+        {
+            if (line[x] == 'T' && line[x + 1] == 'T')
+            {
+                fprintf(fOutput, "\t", line);
+            }
+            else
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    switch (line[x + i])
+                    {
+                    case '0':
+                        pair[i] = 0;
+                        break;
+
+                    case '1':
+                        pair[i] = 1;
+                        break;
+
+                    case '2':
+                        pair[i] = 2;
+                        break;
+
+                    case '3':
+                        pair[i] = 3;
+                        break;
+
+                    case '4':
+                        pair[i] = 4;
+                        break;
+
+                    case '5':
+                        pair[i] = 5;
+                        break;
+
+                    case '6':
+                        pair[i] = 6;
+                        break;
+
+                    case '7':
+                        pair[i] = 7;
+                        break;
+
+                    case '8':
+                        pair[i] = 8;
+                        break;
+
+                    case '9':
+                        pair[i] = 9;
+                        break;
+
+                    case 'A':
+                        pair[i] = 10;
+                        break;
+
+                    case 'B':
+                        pair[i] = 11;
+                        break;
+
+                    case 'C':
+                        pair[i] = 12;
+                        break;
+
+                    case 'D':
+                        pair[i] = 13;
+                        break;
+
+                    case 'E':
+                        pair[i] = 14;
+                        break;
+
+                    case 'F':
+                        pair[i] = 15;
+                        break;
+
+                    default:
+                        fprintf(fOutput, "\n", line);
+                        x = 255;
+                        break;
+                    }
+                    ascii = pair[0] * 16 + pair[1] + 16;
+                    if (ascii > 127)
+                        ascii = (ascii - 144) + 32;
+                }
+                    if(x!=255)
+                    fprintf(fOutput, "%c", ascii);
+
+            }
+            x += 2;
+        }
+    }
+    
+    fclose(fInput);
+    fclose(fOutput);
 }
